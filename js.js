7ba9917944ab9fe5dedf05791a1f5166d566b6ca
karimb76 <karim.bellet@gmail.com>
2018-10-10 10:32:50 +0200

    maj

---
+++ js.js
@@ -0,0 +1,49 @@
+document.getElementById('image1').addEventListener('mouseover', function chang() {
+document.getElementById('image1').src = 'image1_2.jpg';
+}
+)
+
+document.getElementById('image2').addEventListener('mouseover', function chang() {
+document.getElementById('image2').src = 'image2_2.jpg';
+}
+)
+
+document.getElementById('image3').addEventListener('mouseover', function chang() {
+document.getElementById('image3').src = 'image3_2.jpg';
+}
+)
+
+document.getElementById('image4').addEventListener('mouseover', function chang() {
+document.getElementById('image4').src = 'image4_2.jpg';
+}
+)
+
+document.getElementById('image5').addEventListener('mouseover', function chang() {
+document.getElementById('image5').src = 'image5_2.jpg';
+}
+)
+
+document.getElementById('image1').addEventListener('mouseout', function chang() {
+document.getElementById('image1').src = 'image1.jpg';
+}
+)
+
+document.getElementById('image2').addEventListener('mouseout', function chang() {
+document.getElementById('image2').src = 'image2.jpg';
+}
+)
+
+document.getElementById('image3').addEventListener('mouseout', function chang() {
+document.getElementById('image3').src = 'image3.jpg';
+}
+)
+
+document.getElementById('image4').addEventListener('mouseout', function chang() {
+document.getElementById('image4').src = 'image4.jpg';
+}
+)
+
+document.getElementById('image5').addEventListener('mouseout', function chang() {
+document.getElementById('image5').src = 'image5.jpg';
+}
+)
